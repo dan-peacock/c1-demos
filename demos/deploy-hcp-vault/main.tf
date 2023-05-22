eddc7667096627742ff11778457d336ef31cec16
Dan Peacock <32961042+dan-peacock@users.noreply.github.com>
2023-05-22 14:42:55 +0100

    Update main.tf

--- demos/deploy-hcp-vault/main.tf
+++ demos/deploy-hcp-vault/main.tf
@@ -5,7 +5,7 @@ provider "hcp" {
 
 # Create HVN
 resource "hcp_hvn" "example" {
-  hvn_id         =  var.hvn_network
+  hvn_id         =  var.hvn_id
   cloud_provider = "aws"
   region         = var.aws_region
   cidr_block     = "172.25.16.0/20"
@@ -13,7 +13,7 @@ resource "hcp_hvn" "example" {
 
 # Create Vault Cluster
 resource "hcp_vault_cluster" "vault_cluster" {
-  hvn_id     = var.hvn_network
+  hvn_id     = hcp_hvn.example.id
   cluster_id = var.vault_cluster_name
   public_endpoint = true
   tier = var.vault_tier
